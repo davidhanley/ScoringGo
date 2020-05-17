4379ed2f842bb5a29c8353bd79d8bdafa2c88c32
David Hanley <dhanley@groupon.com>
2020-05-17 17:09:17 -0500

    got testing in place

--- scoring_test.go
+++ scoring_test.go
@@ -1,1 +1,25 @@
-package ScoringGo
+package scoring
+
+import "testing"
+
+func TestGetId(t *testing.T) {
+	if GetId("david hanley", 47) != 0 {
+		t.Error("first ID not zero")
+	}
+
+	if GetId("david hanley", 47) != 0 {
+		t.Error("should be same dave")
+	}
+
+	if GetId("david hanley", 48) != 0 {
+		t.Error("should still be same dave")
+	}
+
+	if GetId("david hanley", 46) != 0 {
+		t.Error("should still be same dave")
+	}
+
+	if GetId("david hanley", 0) != 0 {
+		t.Error("should still be same dave")
+	}
+}
